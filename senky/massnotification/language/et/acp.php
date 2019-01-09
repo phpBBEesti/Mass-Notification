3c93b2cedab9fdb65253fc9603092a74c504d6ce
phpBBEesti <argol76@gmail.com>
2019-01-09 11:23:28 +0200

    Update acp.php

--- senky/massnotification/language/et/acp.php
+++ senky/massnotification/language/et/acp.php
@@ -20,21 +20,21 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'MASSNOTIFICATION_DESC'	=> 'Demo Module',
+	'MASSNOTIFICATION_DESC'	=> 'Demo moodul',
 
 	'COMPOSE'				=> 'Koostage massteatis',
 	'ALL_USERS'				=> 'Kõik kasutajad',
 	'SEND_TO_GROUP'			=> 'Saada grupile',
 	'SEND_TO_USERS'			=> 'Saada kasutajatele',
-	'SEND_TO_USERS_EXPLAIN'	=> 'Entering names here will override any group selected above. Enter each username on a new line.',
-	'TITLE'					=> 'Notification title',
-	'MESSAGE_EXPLAIN'		=> 'Keep it short, preferably below 150 characters. You may use HTML.',
-	'URL'					=> 'Redirect URL',
-	'URL_EXPLAIN'			=> 'Enter URL to which user will be redirected after clicking on the notification.',
-	'SEND_NOTIFICATION'		=> 'Send notification',
+	'SEND_TO_USERS_EXPLAIN'	=> 'Nimede sisestamine tühistab kõik ülaltoodud valikud. Sisestage iga kasutajanimi uuele reale.',
+	'TITLE'					=> 'Teatise pealkiri',
+	'MESSAGE_EXPLAIN'		=> 'Hoidke see lühike, eelistatavalt alla 150 tähemärgi. Võite kasutada HTML.',
+	'URL'					=> 'Ümbersuunamise URL',
+	'URL_EXPLAIN'			=> 'Sisestage URL, millele kasutaja pärast teate klõpsamist suunatakse.',
+	'SEND_NOTIFICATION'		=> 'Saada teatis',
 
-	'NO_NOTIFICATION_TITLE'		=> 'You have to specify notification title.',
-	'NO_NOTIFICATION_MESSAGE'	=> 'You have to specify notification message.',
+	'NO_NOTIFICATION_TITLE'		=> 'Peate määrama teate pealkirja.',
+	'NO_NOTIFICATION_MESSAGE'	=> 'Peate määrama teate sõnumi.',
 
-	'NOTIFICATION_SEND'	=> 'Your notification has been sent.',
+	'NOTIFICATION_SEND'	=> 'Teie teade on saadetud.',
 ));
